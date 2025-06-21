73028735ec18870493b37e7adf016fce0236f682
Maru Dhanvi <marudhanvi8@gmail.com>
2025-06-22 00:05:48 +0530

    Add files via upload

---
+++ all (2).cpp
@@ -0,0 +1,136 @@
+#include<iostream>
+using namespace std;
+
+
+class swapping
+{
+     int temp,a,b;
+ public: 
+  void getnum()
+  {
+      cout <<"enter value of a"<< endl;
+      cin >> a;
+      cout << "enter value of b"<< endl;
+      cin >> b;
+  }
+  void displayTemp()
+  {
+      temp=a;
+      a=b;
+      b=temp;
+      cout <<"after swap value of a is:" < <a  <<endl;
+     cout << "after swap value of b is:" << b <<endl;
+    // cout << "value is:"<< temp<<endl;
+  }
+    
+};
+int main()
+{
+    swapping swap;
+    swap.getnum();
+    swap.displayTemp();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
